1b0d98c16ddea9e9098da2944d932d50e5ddc646
BBFifield <bb.fifield@gmail.com>
2024-11-28 23:49:06 -0330

    Renamed config folder to ./nvim and added home-manager module

--- flake.nix
+++ flake.nix
@@ -1,39 +1,27 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
   outputs = {
     nixpkgs,
+    flake-parts,
+    self,
     ...
-  } @ inputs: let
-    systems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in
-  {
-    packages = forAllSystems (system: rec {
-      pkgs = nixpkgs.legacyPackages.${system};
-      default = let 
-        package = pkgs.neovim-unwrapped;
-        config = pkgs.neovimUtils.makeNeovimConfig {
-          customRC = pkgs.writeTextFile {
-            name = "init.lua";
-            text = builtins.readFile ./init.lua;
-          };
-        };
-      in 
-        pkgs.wrapNeovimUnstable package (config);
-    });
-  };
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      perSystem = {pkgs, ...}: {
+        formatter = pkgs.alejandra;
+      };
+      flake = {
+        homeManagerModules.default = import ./nix/hm-module.nix self;
+      };
+    };
 }
-/*environment.systemPackages = with pkgs; [
-    neovim
-  ];
-
-  environment.etc."nvim/init.lua".source = /path/to/your/init.lua;*/
---
+++ nix/hm-module.nix
@@ -0,0 +1,30 @@
+self: {
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  cfg = config.hm.neovimConfig;
+in
+  with lib; {
+    options.hm.neovimConfig = {
+      enable = mkEnableOption "Enable this Neovim configuration.";
+      config = lib.mkOption {
+        type = lib.types.path;
+        default = ../nvim;
+      };
+    };
+    config = mkIf cfg.enable {
+      home.packages = [
+        pkgs.wl-clipboard # For system clipboard capabilities
+        pkgs.ripgrep # For BurntSushi/ripgrep
+        pkgs.gcc # For installing treesitter parsers
+      ];
+
+      # So the entire folder isn't linked to the nix store. This allows you add or replace individual files for testing without having to rebuild all the time.
+      xdg.configFile."nvim" = {
+        source = cfg.config;
+        recursive = true;
+      };
+    };
+  }
