ba8e11bab21a10b1391580cfd09d0c275bd32b87
BBFifield <bb.fifield@gmail.com>
2025-02-10 00:57:42 -0330

    Fix moving to end of line and matching pair keymaps while in insert mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 local create_keymaps = function(maps)
 	for _, v in ipairs(maps) do
 		if type(v[1]) == "table" then
+			-- m is either normal or visual mode
 			for _, m in ipairs(v[1]) do
 				vim.keymap.set(m, v[2], v[3], v[4])
 			end
@@ -9,13 +10,14 @@ local create_keymaps = function(maps)
 	end
 end
 
+-- Creates yank and paste keymaps for every char in both normal and visual modes
 local create_register_keymaps = function()
 	local opts = { silent = true }
 	for i = string.byte("a"), string.byte("z") do
 		local letterReg = string.char(i)
 		local prefix = '"' .. letterReg
 		vim.keymap.set("n", prefix .. "y", prefix .. "y", opts)
-		vim.keymap.set("v", prefix .. "y", prefix .. "ygv", opts)
+		vim.keymap.set("v", prefix .. "y", prefix .. "ygv", opts) -- "gv" means return to visual modeafter executing the command
 		vim.keymap.set("n", prefix .. "Y", prefix .. "Y", opts)
 		create_keymaps({ { { "n", "v" }, prefix .. "p", prefix .. "p", opts } })
 	end
@@ -28,19 +30,35 @@ local create_register_keymaps = function()
 end
 
 local keymaps = {
-	{ "n", "<M-j>", ":m .+1<CR>", { silent = true } },
-	{ "v", "<M-j>", ":m '>+1<CR>gv", { silent = true } },
-	{ "n", "<M-k>", ":m .-2<CR>", { silent = true } },
-	{ "v", "<M-k>", ":m '<-2<CR>gv", { silent = true } },
-	{ "n", "<M-h>", "<<", { silent = true } },
-	{ "v", "<M-h>", "<gv", { silent = true } },
-	{ "n", "<M-l>", ">>", { silent = true } },
-	{ "v", "<M-l>", ">gv", { silent = true } },
-	{ "n", "<leader>bn", ":bn<CR>", { desc = "Next", silent = true } },
-	{ "n", "<leader>bb", ":bp<CR>", { desc = "Prev", silent = true } },
-	{ "n", "<leader>bd", ":bdelete<CR>", { desc = "Close", silent = true } },
-	{ "i", "<M-4>", ":normal $|normal a", { silent = true } }, --Move to end of line in insert mode
-	{ "i", "<M-5>", ":normal %|normal a", { silent = true } }, --Jump to matched pair
+	{ "n", "<M-j>", ":m .+1<CR>", { desc = "Swap line down", silent = true } },
+	{ "v", "<M-j>", ":m '>+1<CR>gv", { desc = "Swap lines down", silent = true } },
+	{ "n", "<M-k>", ":m .-2<CR>", { desc = "Swap line up", silent = true } },
+	{ "v", "<M-k>", ":m '<-2<CR>gv", { desc = "Swap lines up", silent = true } },
+	{ "n", "<M-h>", "<<", { desc = "Move line left", silent = true } },
+	{ "v", "<M-h>", "<gv", { desc = "Move lines left", silent = true } },
+	{ "n", "<M-l>", ">>", { desc = "Move line right", silent = true } },
+	{ "v", "<M-l>", ">gv", { desc = "Move lines right", silent = true } },
+	{ "n", "<leader>bn", ":bn<CR>", { desc = "Next buffer", silent = true } },
+	{ "n", "<leader>bb", ":bp<CR>", { desc = "Prev buffer", silent = true } },
+	{ "n", "<leader>bd", ":bdelete<CR>", { desc = "Close buffer", silent = true } },
+	{
+		"i",
+		"<M-4>",
+		function()
+			vim.cmd("normal $")
+			vim.cmd("startinsert!") -- ! ensures a raw and unaltered switch back to insert mode, which likely preserves the intended cursor position more accurately
+		end,
+		{ silent = true },
+	}, --Move to end of line in insert mode
+	{
+		"i",
+		"<M-5>",
+		function()
+			vim.cmd("normal %")
+			vim.cmd("startinsert!")
+		end,
+		{ silent = true },
+	}, --Jump to matched pair
 }
 
 create_keymaps(keymaps)
